3fb6ffbcf7c75da5f40d4800a4f396e1690709ea
Bhavesh Saluru <bhaveshsaluru9@gmail.com>
2025-10-10 00:15:18 +0530

    Data Manager for talking to the DB

---
+++ src/main/java/com/kudospoints/pointsservice/repository/PointsLedgerRepository.java
@@ -0,0 +1,11 @@
+package com.kudospoints.pointsservice.repository;
+
+import com.kudospoints.pointsservice.domain.PointsLedger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PointsLedgerRepository extends JpaRepository<PointsLedger, UUID> {
+}
